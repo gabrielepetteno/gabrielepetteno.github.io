0f32e7f6371c1c23a795e78be5b2caeb7367a5f1
Gabriele Pettenò <gabri-pette@hotmail.it>
2018-06-06 02:40:56 +0200

    email
    working on email not working

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,40 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
    echo "No arguments Provided!";
    return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'gabrielepetteno@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+}
+echo "WORK GOOD";
+
+include_once("\Mail-1.4.1\Mail.php");
+
+$from = $_POST['email'];
+$to = 'gabrielepetteno@gmail.com';
+$subject = 'Hi!';
+$body = "Hi,\n\n Name: ".$_POST['name']." \n Email: ".$_POST['email']." \n Phone: ".$_POST['phone']." \n Message: ".$_POST['message'];
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'gabrielepetteno.site@gmail.com',
+        'password' => 'GpWebs1te1896!Git'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+echo "WORK GOOD 2";
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+
 ?>
---
+++ mail/example.php
@@ -0,0 +1,20 @@
+<?php
+   echo "I write PHP code like a PRO! 
+         Soon I'll be able to send out emails 
+         using it as well.";
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact form</title>
+</head>
+<body>
+<h1>Contact us</h1>
+<form method="POST">
+    <label for="name">Name: <input type="text" name="name" id="name"></label><br>
+    <label for="email">Email address: <input type="email" name="email" id="email"></label><br>
+    <label for="message">Message: <textarea name="message" id="message" rows="8" cols="20"></textarea></label><br>
+    <input type="submit" value="Send">
+</form>
+</body>
+</html>
