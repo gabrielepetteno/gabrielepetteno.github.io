a82f536dc88c2daa81b293b9b5e6714761352135
gabrielepetteno <gabri-pette@gmail.com>
2018-06-29 14:28:43 +0200

    mail working
    mail is working

--- contact_me.php
+++ contact_me.php
@@ -18,21 +18,7 @@ try {
 	
 	$string = "mailto:gabri-pette@hotmail.it?subject=gabrielepetteno.github.io - ".$email."&body=Hi i'm ".$name."%0D%0AThis is my email : ".$email."%0D%0A This is my phone number : ".$phone."%0D%0A".$message;
 	
-	$to = $email;
-	$subject = "gabrielepetteno.github.io - ".$email;
-	$message = "Hi im ".$name."%0D%0AThis is my email : ".$email."%0D%0A This is my phone number : ".$phone."%0D%0A".$message;
-	//$headers = 'From: gabri-pette@hotmail.it'."\r\n".'Reply-To: gabri-pette@hotmail.it'."\r\n".'X-Mailer: PHP/'.phpversion();
-
-	$header = "From:".$email." \r\n";
-	$header .= "MIME-Version: 1.0\r\n";
-	$header .= "Content-type: text/html\r\n";
-	
-	
-	mail($email, $subject, $message, $headers);
-	
-	//echo "<a target=\"_top\" href=\"mailto:gabri-pette@hotmail.it\">@email@</a>";
-	
-	header("Location:http://gabrielepetteno.github.io");
+	header("Location:$string");
 	
 	
 } catch (Exception $e) {
